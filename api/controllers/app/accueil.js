34b78eb4870b6f1f9833600d3d0c412dbb9c4160
Rodolpho BABATOUNDE <chabibabatounde@gmail.com>
2020-02-15 16:20:39 +0100

    Live terminé

---
+++ api/controllers/app/accueil.js
@@ -0,0 +1,16 @@
+/**
+ * Module dependencies
+ */
+
+// ...
+
+
+/**
+ * app/accueil.js
+ *
+ * Accueil app.
+ */
+module.exports = async function accueil(req, res) {
+  sails.log.debug('TODO: implement');
+  return res.view('app/single-page');
+};
