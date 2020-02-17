546173c52fbe0b991b50f0abf548f07baa0f65ae
Rodolpho BABATOUNDE <chabibabatounde@gmail.com>
2020-02-17 17:45:13 +0100

    Modifications...

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 pm2 stop legibeninradio
-pm2 start /home/serveur-node-js/LEGIBENIN-RADIO/app.js -n legibeninradio -l /home/serveur-node-js/LEGIBENIN-RADIO/log.json --log-type json -- --port 9002
+pm2 start app.js -n legibeninradio -l log.json --log-type json -- --port 9002
 pm2 reload legibeninradio
 pm2 restart legibeninradio
